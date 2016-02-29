e8507ad0b78db12dfe675933b4556cecbfa1e2ff
Lt. <adanmadavid@hotmail.com>
2016-02-29 15:10:19 +0000

    Classes added

---
+++ NewsEffectProj/Companies.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsEffectProj
+{
+    public class Companies
+    {
+        public string name { get; set; }
+    }
+}
---
+++ NewsEffectProj/Staff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsEffectProj
+{
+    public class Staff
+    {
+        public int employeeid { get; set; }
+        public string firstname { get; set; }
+        public string surname { get; set; }
+        public bool ismanager { get; set; }
+    }
+}
