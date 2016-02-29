926713be83472c1767fe2aa06a8ba66cbd84f0c0
Lt. <adanmadavid@hotmail.com>
2016-02-29 12:18:50 +0000

    File Setup

---
+++ NewsEffectRun/Program.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace NewsEffectRun
+{
+    public class Program
+    {
+        private static readonly ILog logger = LogManager.GetLogger("program.cs");
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
